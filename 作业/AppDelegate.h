106a906a93e346a4ccc740516359ef1ceffe7b24
程玉娟 <1009449682@qq.com>
2015-12-03 17:13:34 +0800

    Initial Commit

---
+++ 作业/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  作业
+//
+//  Created by qingyun on 15/12/3.
+//  Copyright (c) 2015年 阿六. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
